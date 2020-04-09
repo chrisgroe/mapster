6d4a4b9f3582a5fee610e48413134d80f5a84419
Christian Gröling <ch.groeling@icloud.com>
2020-04-09 15:55:28 +0200

    Initial commit

---
+++ mapster/mapster.h
@@ -0,0 +1,19 @@
+//
+//  mapster.h
+//  mapster
+//
+//  Created by Christian Gröling on 09.04.20.
+//  Copyright © 2020 Christian Gröling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mapster.
+FOUNDATION_EXPORT double mapsterVersionNumber;
+
+//! Project version string for mapster.
+FOUNDATION_EXPORT const unsigned char mapsterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mapster/PublicHeader.h>
+
+
