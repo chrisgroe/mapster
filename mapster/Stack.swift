0d950c8d596fd843e0c8ae850531dfb63421c1f8
Christian Gröling <ch.groeling@icloud.com>
2020-04-10 10:02:28 +0200

    Refactoring

--- mapster/Stack.swift
+++ mapster/Stack.swift
@@ -8,38 +8,44 @@
 
 import Foundation
 
-/// A Stack (Last-In First-Out) datastructure
+/// A stack (Last-In First-Out) datastructure.
 public struct Stack<T> {
     typealias Element = T
     fileprivate var linkedList : ForwardLinkedList<Element>
     
-    var count: Int {
-        return linkedList.count
-    }
-    
-    var isEmpty: Bool {
-        return linkedList.isEmpty
-    }
-    
+    /// Initializes an empty stack
     init()
     {
         self.linkedList = ForwardLinkedList()
     }
     
+    /// Initializes the stack with a sequence
+    ///
+    /// Element order is [bottom, ..., top], as if one were to iterate through the sequence in reverse.
     init<S>(_ s: S) where Element == S.Element, S : Sequence
     {
         self.linkedList = ForwardLinkedList(s.reversed())
     }
     
+    /// Initializes the stack with variadic parameters
+    ///
+    /// Element order is (bottom, ..., top),  as if one were to iterate through the sequence in reverse.
     init(_ values: Element...) {
         self.linkedList = ForwardLinkedList(values.reversed())
     }
     
+    /// Pushes an element to the top of the stack
+    /// - Parameters:
+    ///     - element: The element to be pushed.
     func push(_ element: Element) {
         linkedList.prepend(element)
     }
-    
-    func pop() -> T? {
+
+    /// Removes the top element from stack and returns it.
+    ///
+    /// This method reduces the size of the stack by 1.
+    /// - Returns:The top element of the stack or nil when the stack is empty.
+    func pop() -> Element? {
         if isEmpty {
             return nil
         }
@@ -47,8 +53,22 @@ public struct Stack<T> {
         return linkedList.removeFirst()
     }
     
-    var front: T? {
+    /// The top element of the stack.
+    ///
+    /// This is the last one pushed to the the stack or nil when the stack is empty.
+    var front: Element? {
         return linkedList.first
     }
+    
+    /// The number of elements currently contained in the stack
+    var count: Int {
+        return linkedList.count
+    }
+    
+    /// True when the stack is empty
+    var isEmpty: Bool {
+        return linkedList.isEmpty
+    }
+    
 }
 
